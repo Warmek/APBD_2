81bf91aa966adf25bc332ea80458c8a6c46984c7
s26225 <s26225@pjwstk.edu.pl>
2024-03-11 18:04:50 +0100

    Added Average func

--- Zadanie_2/Program.cs
+++ Zadanie_2/Program.cs
@@ -2,3 +2,16 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+
+static float Average(int[] input)
+{
+    int sum = 0;
+    int i = 0;
+    foreach (var item in input)
+    {
+        sum += item;
+        i++;
+    }
+    return sum/i;
+}
